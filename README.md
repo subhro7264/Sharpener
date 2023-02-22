77e671326359c6cd662a19da1e82c8a681736978
subhro7264 <subhrohazra7@gmail.com>
2023-02-22 11:51:53 +0530

    web2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Sharpener
 For Work
+<!--Now make ADD ITEM bold and chage the font color to greeen  -->
---
+++ index.js
@@ -0,0 +1,46 @@
+ // Dom 
+// EXAMINE THE DOCUMENT OBJECT
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.url);
+// console.log(document.title);
+// console.log(document.domain);
+// // document.title=123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent="Hello";
+
+// console.log(document.forms);
+// console.log(document.links);
+// console.log(document.images);
+
+// console.log(document.getElementById("header-title"))
+
+// var headerTitle=document.getElementById("header-title");
+// var header=document.getElementById("main-header");
+// console.log(headerTitle);
+// headerTitle.textContent='hello';
+// headerTitle.innerText= "Goodbye";
+// console.log(headerTitle.innerText);
+
+// headerTitle.innerHTML="<h3>Hello</h3>";
+// headerTitle.style.borderBottom="solid 3px #000"
+// header.style.borderBottom="solid 3px #000"
+
+
+// Now make ADD ITEM bold and chage the font color to greeen
+
+var ad=document.getElementById("title");
+ad.style.fontVariant ="small-caps";
+ad.style.color="green";
+
+
+
+
+
+
+
