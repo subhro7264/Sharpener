800bb87629677bdc8a239f18293570f25bc894b0
subhro7264 <subhrohazra7@gmail.com>
2023-02-27 10:01:49 +0530

    Delete-data-from-Local-Storage

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Sharpener
 
-Lets-Scale-the-app-to-more-users
+<!-- Add a Delete Button - I want to be able to delete a few of them from the local storage as well as the UI because they are not interested in our product maybe. -->
 
 
 
--- script.js
+++ script.js
@@ -1,4 +1,53 @@
 
+// var from = document.getElementById("form-head");
+
+// from.addEventListener("submit", saveLocalStroage);
+
+// function saveLocalStroage(e) {
+//     e.preventDefault();
+//     const name = e.target.fullname.value;
+//     const phone = e.target.number.value;
+//     const email = e.target.emailId.value;
+//     const dateToCall = e.target.date_to_call.value;
+//     const obj = {
+//         name,
+//         email,
+//         phone,
+//         dateToCall
+//     }
+//     localStorage.setItem(obj.name, JSON.stringify(obj))
+//     showOnTheScreen(obj)
+   
+// }
+
+// function showOnTheScreen(obj) {
+//     const parantElement = document.getElementById("listOfItem");
+//     const childElement = document.createElement("li");
+//     childElement.textContent = childElement.textContent + `${obj.name } - ${obj.email}-${obj.dateToCall}-${obj.Phone}`;
+//     parantElement.appendChild(childElement);
+
+//     // const deleteBtn=document.createElement("button");
+//     // deleteBtn.className = "delete";
+//     // deleteBtn.appendChild(document.createTextNode("DELETE"));
+//     // li.appendChild(deleteBtn);
+//     // removeItem(e)
+// };
+
+
+
+
+// // parantElement.addEventListener("click", removeItem);
+// // // delete
+// // function removeItem(e) {
+// //     if (e.target.classList.contains("delete")) {
+// //         if (confirm("ARE YOU SURE ?")) {
+// //             const li = e.target.parentElement;
+// //             childElement.removeChild(li);
+// //         }
+// //     }
+// // };
+
+
 var from = document.getElementById("form-head");
 
 from.addEventListener("submit", saveLocalStroage);
@@ -16,14 +65,22 @@ function saveLocalStroage(e) {
         dateToCall
     }
     localStorage.setItem(obj.name, JSON.stringify(obj))
-    showOnTheScreen(obj)
+    showOnTheScreen(obj);
 
 }
 
 function showOnTheScreen(obj) {
-    const parantElement = document.getElementById("listOfItem");
-    const childElement = document.createElement("li");
-    childElement.textContent = childElement.textContent + `${obj.name } - ${obj.email}-${obj.Phone}${obj.dateToCall}`;
-    parantElement.appendChild(childElement);
-
-};
+    const parantElm = document.getElementById("listOfItem");
+    const childElm = document.createElement("li");
+    childElm.textContent = childElm.textContent + `${obj.name } - ${obj.email}-${obj.Phone}${obj.dateToCall}`;
+   const deleteBtn=document.createElement("input");
+   deleteBtn.type="button";
+   deleteBtn.value="Delete";
+   deleteBtn.onclick=()=>{
+    localStorage.removeItem(obj.name)
+    parantElm.removeChild(childElm)
+   }
+    childElm.appendChild(deleteBtn);
+    parantElm.appendChild(childElm);
+}
+ 
